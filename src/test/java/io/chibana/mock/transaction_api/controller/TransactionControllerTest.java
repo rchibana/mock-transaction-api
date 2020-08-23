9214e35b93bb2f46136f03ac95431cc0a9abd428
rchibana <rodrigo.chibana@gmail.com>
2020-08-23 17:25:34 -0300

    Remove unnecessary class

--- src/test/java/io/chibana/mock/transaction_api/controller/TransactionControllerTest.java
+++ src/test/java/io/chibana/mock/transaction_api/controller/TransactionControllerTest.java
@@ -1,6 +1,5 @@
 package io.chibana.mock.transaction_api.controller;
 
-import io.chibana.mock.transaction_api.TestHelper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,7 +8,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultMatcher;
-import org.springframework.util.MultiValueMap;
 
 import java.io.UnsupportedEncodingException;
 
